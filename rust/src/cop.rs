8f30b4c108d244bbb9548911596e0a5fe68d34aa
Amadeu Marques <maramadeu@gmail.com>
2023-12-04 15:23:30 +0000

    view transfer metrics

--- rust/src/cop.rs
+++ rust/src/cop.rs
@@ -156,6 +156,7 @@ pub fn main() {
                 with_metrics(atlas_communication::metric::metrics()),
                 with_metrics(atlas_smr_replica::metric::metrics()),
                 with_metrics(atlas_log_transfer::metrics::metrics()),
+                with_metrics(atlas_view_transfer::metrics::metrics()),
                 with_metrics(atlas_divisible_state::metrics::metrics()),
                 with_metrics(progressive_state_transfer::stp::metrics::metrics()),
                 with_metric_level(MetricLevel::Info),
