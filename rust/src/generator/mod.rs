2639a6e560c0e0ef257334050f5069d4b014f440
Amadeu Marques <maramadeu@gmail.com>
2024-02-21 16:53:58 +0000

    50/50

--- rust/src/generator/mod.rs
+++ rust/src/generator/mod.rs
@@ -17,7 +17,7 @@ const ZIPF_CONSTANT: f64 = 0.0;
 
 pub const NUM_KEYS: usize = 4096000;
 const INSERT_OPS: u32 = 0;
-const READ_OPS: u32 = 0;
+const READ_OPS: u32 = 1;
 const REMOVE_OPS: u32 = 0;
 const UPDATE_OPS: u32 = 1;
 
