9474f32bf8cd98659a25160dd64674a73f0130bd
Amadeu Marques <maramadeu@gmail.com>
2024-03-20 17:02:47 +0000

    monolithic 95 5 4gb

--- rust/src/exec.rs
+++ rust/src/exec.rs
@@ -25,7 +25,7 @@ fn initial_state() -> Result<StateOrchestrator> {
 
     let path = format!("{}{}", "./appdata_",id);
 
-    let mut state = StateOrchestrator::new(&path, 16);
+    let mut state = StateOrchestrator::new(&path);
     
     Ok(state)
 }
