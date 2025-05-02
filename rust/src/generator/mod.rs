429155694276c18df325f97e33774aa5c0b7e0ef
Amadeu Marques <maramadeu@gmail.com>
2025-05-02 19:22:47 +0100

    just keys

--- rust/src/generator/mod.rs
+++ rust/src/generator/mod.rs
@@ -12,7 +12,7 @@ use crate::common::{get_insert_ops, get_num_keys, get_read_ops, get_remove_ops, 
 pub const PRIMARY_KEY_LEN: usize = 32;
 const SECONDARY_KEY_LEN: usize = 32;
 const VALUE_LEN: usize = 256;
-const HASHMAP_LEN: usize = 10;
+const HASHMAP_LEN: usize = 0;
 
 // for more "randomness" in the distribution this should be between  ]0.0,0.24[
 const ZIPF_CONSTANT: f64 = 0.0;
