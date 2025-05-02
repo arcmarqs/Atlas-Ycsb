7e70aacd300986c5411ca3b12ccad452f58ace00
Amadeu Marques <maramadeu@gmail.com>
2025-05-02 11:07:32 +0100

    change hashmap value len

--- rust/src/generator/mod.rs
+++ rust/src/generator/mod.rs
@@ -11,7 +11,7 @@ use crate::common::{get_insert_ops, get_num_keys, get_read_ops, get_remove_ops, 
 
 pub const PRIMARY_KEY_LEN: usize = 32;
 const SECONDARY_KEY_LEN: usize = 16;
-const VALUE_LEN: usize = 64;
+const VALUE_LEN: usize = 128;
 const HASHMAP_LEN: usize = 10;
 
 // for more "randomness" in the distribution this should be between  ]0.0,0.24[
